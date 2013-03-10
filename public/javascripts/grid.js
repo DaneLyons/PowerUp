98a8db21094278b449742f042f02cb9b6a898d16
Dane Lyons <duilen@gmail.com>
2013-03-10 16:11:29 -0700

    large grid

--- public/javascripts/grid.js
+++ public/javascripts/grid.js
@@ -5,12 +5,12 @@ $(function(){
   var grid_size = window_height-115;
   var grid = $('#grid');
   grid.css({width:grid_size,height:grid_size}); 
-  for(var i=0;i<25;i++){
+  for(var i=0;i<400;i++){
     grid.append($('<li><div></div></li>'));
   }
-  for(var i=0;i<10;i++){
+  for(var i=0;i<40;i++){
     setTimeout(function(){
-      var rand_square = Math.floor(Math.random() * 25);
+      var rand_square = Math.floor(Math.random() * 400);
       grid.find('li:eq('+rand_square+')').addClass('active');
     },(i+1)*75)
   }
---
+++ public/javascripts/home.js
@@ -0,0 +1,4 @@
+$(function(){
+  var window_height = $(window).height();
+  $('#page').css({height:window_height});
+});
