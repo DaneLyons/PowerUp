cd5a5b10464ed7e1bd2646ad45803383a27d0447
Dane Lyons <duilen@gmail.com>
2013-03-30 16:45:11 -0700

    random use cases

--- public/js/slides.js
+++ public/js/slides.js
@@ -1,3 +1,18 @@
+var useCases = [
+  { goal:"Lose Weight", units:["10 Push-Ups","20 Sit-Ups","Run 10 Minutes"] },
+  { goal:"Become a Better Writer", units:["Write 15 Minutes","Read 15 Minutes","Study 15 Minutes"] },
+  { goal:"Eat Healthier", units:["Cook a Meal","Drink Glass of Water","Refuse Dessert"] },
+  { goal:"Get Stronger", units:["Weights: 10 Reps","Cardio: 5 Minutes","Sports: 30 Minutes"] },
+  { goal:"Become a Better Programmer", units:["Code: 15 Minutes","Study: 15 Minutes","Write Blog Post"] },
+  { goal:"Learn to Play Guitar", units:["Play: 15 Minutes","Study: 15 Minutes","Write Lyrics"] },
+  { goal:"Become a Blogger", units:["Write Blog Post","Write Comment","Read Blog Post"] },
+  { goal:"Allowance Grid", units:["Clean Room","Do Dishes","Take out Trash"] },
+  { goal:"Game Leaderboard", units:["John Wins","Sarah Wins","Tie"] },
+  { goal:"Save Money", units:["Cheap Dinner","Purchased Discount Item","Put $15 in Savings"] },
+  { goal:"Become More Creative", units:["Documented Idea","Read: 30 Minutes","Art Project: 15 Minutes"] },
+  { goal:"Be More Informed", units:["Read News: 15 Minutes","Study: 15 Minutes","Talk to Representative"] }
+]
+
 $(function(){
   $('.toc button').click(function(){
     var elem = $(this);
@@ -24,5 +39,19 @@ $(function(){
     ev.preventDefault();
     var slide = $(this).data('slide');
     $('.toc button.'+slide).trigger('click');
+  });
+  
+  $('button.clear').click(function(ev){
+    ev.preventDefault();
+    $('.work_unit,.goal').val('');
+  });
+  
+  $('button.random').click(function(ev){
+    ev.preventDefault();
+    var useCase = useCases[Math.floor(Math.random() * useCases.length)];
+    $('input.goal').val(useCase.goal);
+    $('input.work_unit:eq(0)').val(useCase.units[0]);
+    $('input.work_unit:eq(1)').val(useCase.units[1]);
+    $('input.work_unit:eq(2)').val(useCase.units[2]);
   });
 });
