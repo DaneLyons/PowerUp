dd9d429b073ba99c8c069512908a6059103a6d39
Dane Lyons <duilen@gmail.com>
2013-03-30 19:06:14 -0700

    user count update

--- routes/page.js
+++ routes/page.js
@@ -6,7 +6,7 @@ exports.home = function (req, res) {
   User.count({},function(err,count){
     var freebies = 500-count;
     if(freebies<0){ freebies=0 }
-    
+    freebies += ""; //makes freebies a string
     res.render("page/home.ejs", {
       "freebies":freebies,
       "stylesheets":["home"],
