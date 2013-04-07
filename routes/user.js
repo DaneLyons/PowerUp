3d0f6382324f33b86471495bdeb24332b93cea91
Dane Lyons <duilen@gmail.com>
2013-04-07 13:56:53 -0700

    settings form

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,15 @@ var Grid = require('../models/grid'),
   inflect = require('i')();
   
 exports.settings = function (req, res) {
-  res.render("user/settings.ejs", {
-    "title":"PowerUp Settings",
-    "stylesheets":["page","settings"]
+  if (typeof req.user === 'undefined') {
+    res.redirect('/');
+    return;
+  }
+  User.findById(req.user._id, function (err, user) {
+    res.render("user/settings.ejs", {
+      "title":"PowerUp Settings",
+      "stylesheets":["page","settings"],
+      "user":user
+    });
   });
 }
