29d751ca1f5ec0c6d962dfbb3c25b311fcdaa2e0
Austin J. Pocus <ajpocus@gmail.com>
2013-04-06 18:15:32 -0700

    jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -1,15 +1,15 @@
 desc('Install required npm modules');
 task('install-npm-depends', [], function () {
-
+	
 	console.log("\n\n > Attempting to install dependencies via npm\n".blue);
 
 	// Get the fs module
 	var spawn = require('child_process').spawn;
-
+	
 	console.log("    Executing command:\n    $ npm install\n".grey);
 
 	npm = spawn("npm", ["install"]);
-
+	
 	npm.stdout.on('data', function (data) {
 		process.stdout.write(("    " + data).grey);
 	});
@@ -38,21 +38,25 @@ desc('Loads in properties file');
 task('load-props', ["install-npm-depends"], function() {
 
 	console.log("\n\n > Attempting to read in build properties\n".blue);
-
+	
 	var fs = require("fs");
-
+	
 	// Read in and parse build properties
-	properties = JSON.parse(fs.readFileSync('config/props.json'));
-
+	properties = JSON.parse(fs.readFileSync('package.json'));
+	
 	// Print the properties to the console
 	for (var p in properties) {
 		if (properties.hasOwnProperty(p)) {
 			console.log(("    " + p + " : " + properties[p]).grey);
 		}
 	}
-
+	
+	properties.siteName = "powerup";
+	properties.siteLocation = "/var/www/powerup/";
+	properties.state = "live";
+	
 	// Build some paths from properties for use later on
-	versionedPath = properties.siteLocation + properties.state + "/.versions/" +
+	versionedPath = "/var/www/powerup/live/.versions/" +
 									properties.siteName + "@" + properties.version + "-" + new Date().getTime();
 	livePath = properties.siteLocation + properties.state + "/" + properties.siteName;
 
@@ -64,12 +68,12 @@ task('load-props', ["install-npm-depends"], function() {
 
 desc('Create versioned site directory');
 task('create-versioned-dir', ["load-props"], function() {
-
+	
 	console.log("\n\n > Attempting to create versioned directory\n".blue);
 
 	var exec = require('child_process').exec,
 			mkdir;
-
+	
 	console.log(("    Executing command:\n    $ mkdir " + versionedPath + "\n").grey);
 	// Create versioned directory
 	mkdir = exec("mkdir " + versionedPath, function (error, stdout, stderr) {
@@ -94,7 +98,7 @@ task('move-files', ["load-props", "create-versioned-dir"], function() {
 			rsync;
 
 	console.log(("    Executing command:\n    $ rsync -a . " + versionedPath).grey);
-
+	
 	// Move files from temporary directory to versioned directory just created
 	rsync = exec("rsync -a . " + versionedPath, function (error, stdout, stderr) {
 		if (error !== null) {
@@ -115,7 +119,7 @@ task('symlink-live', ["load-props", "create-versioned-dir", "move-files"], funct
 
 	var exec = require('child_process').exec,
 			ln;
-
+	
 	console.log(("    Executing command:\n    $ rm " + livePath + " && ln -sv " + versionedPath + " " + livePath).grey);
 	// Symlink to the versioned directory
 	ln = exec("rm " + livePath + " && ln -sv " + versionedPath + " " + livePath, function (error, stdout, stderr) {
@@ -138,18 +142,18 @@ task('default', ["load-props", "create-versioned-dir", "move-files", "symlink-li
 	var exec = require('child_process').exec,
 			spawn = require('child_process').spawn,
 			upstart;
-
-	console.log(("    Executing command:\n    $ sudo monit stop " + properties.siteName + "-" + properties.state).grey);
+	
+	console.log(("    Executing command:\n    $ monit stop " + properties.siteName + "-" + properties.state).grey);
 	// Stop the old version of the app and start the new version with monit
-	exec("sudo monit stop " + properties.siteName + "-" + properties.state, function (error, stdout, stderr) {
+	exec("monit stop " + properties.siteName + "-" + properties.state, function (error, stdout, stderr) {
 
 		if (error) {
 			throw error;
 		} else {
-			console.log(("\n    Executing command:\n    $sudo monit start " +
+			console.log(("\n    Executing command:\n    $ monit start " +
 												properties.siteName + "-" + properties.state).grey);
 
-			exec("sudo monit start " + properties.siteName + "-" + properties.state, function (error, stdout, stderr) {
+			exec("monit start " + properties.siteName + "-" + properties.state, function (error, stdout, stderr) {
 
 				console.log("\n + Old instance killed successfully\n".green);
 				if (error) {
