2b6856ca4710c307553dd4741c1b0d9b1ed0be7c
Austin J. Pocus <ajpocus@gmail.com>
2013-06-09 16:34:22 -0700

    better subject

--- models/invite.js
+++ models/invite.js
@@ -25,36 +25,44 @@ inviteSchema.plugin(timestamps);
 
 inviteSchema.post('save', function (invite) {
   if (!invite.isSent) {
-    Grid.findById(invite.grid, function (err, grid) {
-      if (err) { console.log(err); }
-
-      User.findOrCreate({email: invite.toParams.email}, function (err, user) {
+    Invite.findById(invite._id, function (err, invite) {
+      Grid.findById(invite.grid, function (err, grid) {
         if (err) { console.log(err); }
 
-        var baseUrl = "http://" + ((process.env.NODE_ENV === 'production') ? 'powerup.io' : 'localhost:3000');
-        var inviteUrl = baseUrl + "/invites/" + invite._id;
-        inviteUrl += "?token=" + invite.token;
-
-        var mailerParams = {
-          to: user.email,
-          subject: "You're invited to a grid on PowerUp.io.",
-          text: ["Hi there,",
-            "",
-            "You've been invited to the \"" + grid.name + "\" grid on PowerUp.io. To get started, click the link below:",
-            "",
-            inviteUrl,
-            "",
-            "Power on!",
-            "- Team PowerUp"].join('\n')
-        };
-
-        Mailer.send(mailerParams, function (err) {
+        User.findOrCreate({email: invite.toParams.email}, function (err, user) {
           if (err) { console.log(err); }
 
-          invite.isSent = true;
-          invite.toUser = user._id;
-          invite.save(function (err) {
+          var baseUrl = "http://" + ((process.env.NODE_ENV === 'production') ? 'powerup.io' : 'localhost:3000');
+          var inviteUrl = baseUrl + "/invites/" + invite._id;
+          inviteUrl += "?token=" + invite.token;
+
+          var subject = "";
+          if (invite.fromUser.name) {
+            subject = invite.fromUser.name + "wants to collaborate with you.";
+          } else {
+            subject = "You've been invited to collaborate.";
+          }
+          var mailerParams = {
+            to: user.email,
+            subject: subject,
+            text: ["Hi there,",
+              "",
+              "You've been invited to the \"" + grid.name + "\" grid on PowerUp.io. To get started, click the link below:",
+              "",
+              inviteUrl,
+              "",
+              "Power on!",
+              "- Team PowerUp"].join('\n')
+          };
+
+          Mailer.send(mailerParams, function (err) {
             if (err) { console.log(err); }
+
+            invite.isSent = true;
+            invite.toUser = user._id;
+            invite.save(function (err) {
+              if (err) { console.log(err); }
+            });
           });
         });
       });
