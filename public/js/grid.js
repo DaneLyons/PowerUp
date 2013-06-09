e510c62f49fa53bafc93c8b6cfa252c7c6f68bf7
Austin J. Pocus <ajpocus@gmail.com>
2013-06-09 16:54:02 -0700

    oop

--- public/js/grid.js
+++ public/js/grid.js
@@ -38,6 +38,26 @@ $(function () {
       
       legend_buttons.click(function(){
       });
+    }, 
+    gridMilestone: function gridMilestone(){
+      var grid = $('#grid');
+      var grid_pos = grid.offset();
+      var grid_width = grid.width();
+      var grid_height = grid.height();
+      var percent_complete = $('.progress').text();
+      var milestone = $('<div id="milestone">\
+        <h1>'+percent_complete+' complete!</h1>\
+        <h2><img src="/img/icon_white.png" /></h2>\
+      </div>');
+      $('body').append(milestone);
+      milestone.css({left:grid_pos.left,width:grid_width,height:(grid_height/4)});
+      milestone.animate({top:(grid_pos.top+(grid_height/3))},1000,"easeOutBounce",function(){
+        setTimeout(function(){
+          milestone.animate({opacity:0},1000,function(){
+            milestone.remove();
+          });
+        },5000)
+      });
     },
     populateGrid: function (grid) {
       var powerUpCount = grid.powerUps.length;
@@ -131,26 +151,6 @@ $(function () {
     gridKeeper: function() {
       $('.legend button').on('click', function(ev) {
         console.log('GRID KEEPER');
-      });
-    },
-    gridMilestone: function gridMilestone(){
-      var grid = $('#grid');
-      var grid_pos = grid.offset();
-      var grid_width = grid.width();
-      var grid_height = grid.height();
-      var percent_complete = $('.progress').text();
-      var milestone = $('<div id="milestone">\
-        <h1>'+percent_complete+' complete!</h1>\
-        <h2><img src="/img/icon_white.png" /></h2>\
-      </div>');
-      $('body').append(milestone);
-      milestone.css({left:grid_pos.left,width:grid_width,height:(grid_height/4)});
-      milestone.animate({top:(grid_pos.top+(grid_height/3))},1000,"easeOutBounce",function(){
-        setTimeout(function(){
-          milestone.animate({opacity:0},1000,function(){
-            milestone.remove();
-          });
-        },5000)
       });
     }
   }
