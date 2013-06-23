706ed6e0df984f4b6b972314b56e0a4bc3beb354
Dane Lyons <duilen@gmail.com>
2013-06-23 11:53:48 -0700

    grid intro

--- public/js/grid.js
+++ public/js/grid.js
@@ -166,4 +166,13 @@ $(function () {
     $(".sections .section").hide();
     $(".sections ."+section).show();
   });
+  
+  $('#demo button').on('click',function(){
+    $('#demo_screen').animate({ opacity:0 },300,function(){
+      $(this).remove();
+    });
+    $('#demo').animate({ top:-500 },300,function(){
+      $(this).remove();
+    });
+  });
 });
