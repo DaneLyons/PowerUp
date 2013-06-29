4c5deef877f0ad84ee9dd39afec090a6c49a4dc6
Dane Lyons <duilen@gmail.com>
2013-06-28 21:49:59 -0700

    sample popup

--- public/js/grid.js
+++ public/js/grid.js
@@ -7,4 +7,19 @@ $(function () {
       $(this).remove();
     });
   });
+  
+  $('#grid li').on('click',function(){
+    var elem = $(this);
+    if(elem.hasClass('active')){
+      $('#popup').remove();
+      var popup = $('<div id="popup"><div class="arrow"></div><h1>PowerUp</h1></div>');
+      //add content to popup
+      var content = $('<p>Sample content</p><p>Sample content</p>');
+      popup.append(content);
+      
+      var offset = elem.offset();
+      popup.css({ left:(offset.left+elem.width()+14), top:(offset.top-26) });
+      $('body').append(popup);
+    }
+  });
 });
