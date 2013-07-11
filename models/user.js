18994d9cb57750dcaa887cbfbb6db71fe6611094
Austin J. Pocus <ajpocus@gmail.com>
2013-07-10 23:46:59 -0700

    basic auth strategy

--- models/user.js
+++ models/user.js
@@ -13,6 +13,7 @@ var userSchema = new Schema({
   email: String,
   passwordHash: String,
   passwordSalt: String,
+  secret: String,
   isConfirmed: { type: Boolean, default: true },
   grids: [ { type: Schema.ObjectId, ref: 'Grid' } ],
   promo: {
@@ -32,6 +33,12 @@ userSchema.pre('save', function (next) {
   }
   
   next();
+});
+
+userSchema.pre('save', function (next) {
+  if (!this.secret) {
+    this.secret = uuid.v4();
+  }
 });
 
 userSchema.statics.bcryptPassword = function (userParams, done) {
